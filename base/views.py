cdb1bf2733659df4cd1102a355158caa74d2c6b2
Exitroz <ezeobih@gmail.com>
2024-02-13 19:21:32 +0100

    template setup contd

--- base/views.py
+++ base/views.py
@@ -8,3 +8,13 @@ class home(ListView):
     template_name = 'index.html'
     context_object_name = 'home'
 
+class about(ListView):
+    model = YourModel
+    template_name = 'about.html'
+    context_object_name = 'about'
+
+class gallery(ListView):
+    model = YourModel
+    template_name = 'gallery.html'
+    context_object_name = 'gallery'
+
